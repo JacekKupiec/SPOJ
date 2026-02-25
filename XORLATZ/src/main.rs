50035cfb10cd6a9422c73d05fd2e3ef677ee7a58
Jacek Kupiec <jacek.kupiec@o2.pl>
2026-02-25 22:18:50 +0100

    first attempt to Collatz polynomial

--- XORLATZ/src/main.rs
+++ XORLATZ/src/main.rs
@@ -1,3 +1,12 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = std::io::stdin().lock();
+    let mut buffer = String::with_capacity(1024);
+
+    _ = input.read_to_string(&mut buffer).unwrap();
+
+    let p = buffer.trim_end().parse::<u32>().unwrap();
+
+    println!("42");
 }
